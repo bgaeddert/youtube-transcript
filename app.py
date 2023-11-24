5e3291483263ccf2d1e8c547a5bf86a5a531eb81
Brian Gaeddert <bgaeddert@gmail.com>
2023-11-24 09:39:07 -0600

    Remove debug mode from app.run()

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def video_deatils():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True, host='0.0.0.0')
+    app.run(port=5000, host='0.0.0.0')
